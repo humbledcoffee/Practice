9f48fdee461fa65611dadce094eb0609039f007c
咖啡要加糖 <594704005@qq.com>
2023-08-10 14:05:12 +0800

    更新笔记

--- es6-11新特性/1.js
+++ es6-11新特性/1.js
@@ -4,6 +4,7 @@ function A1() {
 function A2() {
   console.log("A2");
 }
+console.log(import.meta);
 export default {
   A1,
   A2,
---
+++ es6-11新特性/2.js
@@ -0,0 +1,10 @@
+function B1() {
+  console.log("B1");
+}
+function B2() {
+  console.log("B2");
+}
+export default {
+  B1,
+  B2,
+};
