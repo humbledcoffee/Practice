936c2ce58fe825452e627740d3af0337c0ce01ca
咖啡要加糖 <594704005@qq.com>
2023-08-13 17:52:55 +0800

    清除缓存 生效ignore

--- nodeJs/Http和url/cors.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "application/json;charset=utf-8");
-  res.setHeader("access-control-allow-origin", "*");
-});
-server.listen("8080", () => {
-  console.log("cors");
-});
