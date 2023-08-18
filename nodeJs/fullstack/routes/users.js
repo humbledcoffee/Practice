fecff4e41bb1b87ecd32f801384130f798a67e8b
咖啡要加糖 <594704005@qq.com>
2023-08-18 16:09:10 +0800

    增删改查搭建完成

--- nodeJs/fullstack/routes/users.js
+++ nodeJs/fullstack/routes/users.js
@@ -5,7 +5,8 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
-router.post("/user/add", (req, res) => {
+//增
+router.post("/user", (req, res) => {
   console.log(req.body);
   const { username, password, age } = req.body;
   //插入数据库
@@ -23,5 +24,41 @@ router.post("/user/add", (req, res) => {
     .catch((err) => {
       console.log(err);
     });
+});
+//删
+router.delete("/user/:id", (req, res) => {
+  UserModel.deleteOne({ _id: req.params.id }).then(() => {
+    res.send({
+      ok: 1,
+    });
+  });
+});
+//改
+router.put("/user/:id", (req, res) => {
+  console.log(req.body, req.params.id);
+  const { username, password, age } = req.body;
+  //插入数据库
+  //1.创建一个模型 user 限制插入类型 一一对应数据库的集合
+  UserModel.updateOne({ _id: req.params.id }, { username, password, age })
+    .then((data) => {
+      res.send({
+        ok: 1,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+//查
+router.get("/user", (req, res) => {
+  // find 第一个参数是一个对象
+  // 第二个参数数组可以控制返回什么名字的域
+  // 可以链式调用
+  // sort({ age: 1 })可以排序
+  // skip 从第几个开始取
+  // limit 要几个数据
+  UserModel.find().then((data) => {
+    res.send(data);
+  });
 });
 module.exports = router;
