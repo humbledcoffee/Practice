856b7b95ac7ef8d9eae04992c976aa709e8c14c0
咖啡要加糖 <594704005@qq.com>
2023-10-28 17:17:56 +0800

    静态资源

---
+++ 后端/helloWord.java
@@ -0,0 +1,16 @@
+class hello {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        double radius1 = 1.2;
+        double radius2 = 2.5;
+        int radius3 = 6;
+
+        double area1 = pi * radius1 * radius1;
+        double area2 = pi * radius1 * radius2;
+        double area3 = pi * radius3 * radius3;
+        System.out.println("面积为" + area1);
+        System.out.println("面积为" + area2);
+        System.out.println("面积为" + area3);
+
+    }
+}
