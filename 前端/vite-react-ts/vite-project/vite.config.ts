9a3e55f91615d1792782f3fa80b4d966c9b4a4de
咖啡要加糖 <594704005@qq.com>
2024-03-05 11:27:42 +0800

    样式方案提交

--- 前端/vite-react-ts/vite-project/vite.config.ts
+++ 前端/vite-react-ts/vite-project/vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import autoprefixer from "autoprefixer";
+import { defineConfig, normalizePath } from "vite";
+// 如果类型报错，需要安装 @types/node: pnpm i @types/node -D
+import react from "@vitejs/plugin-react-swc";
+// 全局 scss 文件的路径
+// 用 normalizePath 解决 window 下的路径问题
+const variablePath = normalizePath(path.resolve("./src/variable.scss"));
+import UnoCss from "unocss/vite";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // 手动指定项目根目录位置
+  // root: path.join(__dirname, 'src')
+  plugins: [react(), UnoCss()],
+  // css 相关的配置
+  css: {
+    modules: {
+      // 一般我们可以通过 generateScopedName 属性来对生成的类名进行自定义
+      // 其中，name 表示当前文件名，local 表示类名
+      generateScopedName: "[name]__[local]__[hash:base64:5]",
+    },
+    //预处理配置
+    preprocessorOptions: {
+      scss: {
+        // additionalData 的内容会在每个 scss 文件的开头自动注入
+        additionalData: `@import "${variablePath}";`,
+      },
+    },
+    // 进行 PostCSS 配置
+    postcss: {
+      plugins: [
+        autoprefixer({
+          // 指定目标浏览器
+          overrideBrowserslist: ["Chrome > 40", "ff > 31", "ie 11"],
+        }),
+      ],
+    },
+  },
+});
